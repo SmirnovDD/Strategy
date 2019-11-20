d53adcc7486d883d9f6bf84881ce8b670158c053
SmirnovDD <studioindevelopment@gmail.com>
2019-11-20 13:28:30 +0300

    Ragdolls

--- Assets/Scripts/AI/UnitHealth.cs
+++ Assets/Scripts/AI/UnitHealth.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class UnitHealth : MonoBehaviour
 {
+    public GameObject unitRagdoll;
     public float startingHP;
     private float hp;
     // Start is called before the first frame update
@@ -12,16 +13,19 @@ public class UnitHealth : MonoBehaviour
         hp = startingHP;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void TakeDamage(float damage)
     {
         hp -= damage;
         if (hp <= 0)
+        {
+            if (unitRagdoll)
+            {
+                GameObject newRagdoll = Instantiate(unitRagdoll, transform.position, transform.rotation);
+                Rigidbody ragdollRigidB = newRagdoll.GetComponentInChildren<Rigidbody>();
+                ragdollRigidB.AddForce(-transform.forward * 1000 + Vector3.up * 1000);
+            }
             Destroy(gameObject);
+        }
     }
 }
